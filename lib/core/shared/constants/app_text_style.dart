f730fdbc451819d61175f550a699f066b2d9b43c
vitu <vitorcsharp@hotmail.com>
2023-06-23 10:09:34 -0300

    add new styles

--- lib/core/shared/constants/app_text_style.dart
+++ lib/core/shared/constants/app_text_style.dart
@@ -3,9 +3,15 @@ import 'package:flutter/material.dart';
 class AppTextStyle {
   AppTextStyle._();
 
-  static const TextStyle bigText = TextStyle(
-    fontSize: 50,
-    fontWeight: FontWeight.w700,
-    fontFamily: "Inter"
-  );
+  static const TextStyle bigText =
+      TextStyle(fontSize: 50, fontWeight: FontWeight.w700, fontFamily: "Inter");
+
+  static const TextStyle mediumText =
+      TextStyle(fontSize: 28, fontWeight: FontWeight.w700, fontFamily: "Inter");
+
+  static const TextStyle smallText =
+      TextStyle(fontSize: 14, fontWeight: FontWeight.w500, fontFamily: "Inter");
+
+  static const TextStyle buttonText =
+      TextStyle(fontSize: 18, fontWeight: FontWeight.w600, fontFamily: "Inter");
 }
