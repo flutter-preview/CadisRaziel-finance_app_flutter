0234067007cc121a3cd9fb8200d6bffa456e3c10
vitu <vitorcsharp@hotmail.com>
2023-06-24 16:43:07 -0300

    add new color

--- lib/core/shared/constants/app_colors.dart
+++ lib/core/shared/constants/app_colors.dart
@@ -15,5 +15,7 @@ class AppColors {
   ];
   static const Color white = Color(0xFFFFFFFF);
   static const Color iceWhite = Color(0xFFEEF8F7);
-  static const Color grey = Color(0xFF444444);
+  static const Color darkGrey = Color(0xFF444444);
+  static const Color grey = Color(0xFF666666);
+  static const Color red = Color(0xFFFA0000);
 }
