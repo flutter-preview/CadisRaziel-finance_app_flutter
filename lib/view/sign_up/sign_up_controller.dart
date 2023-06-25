9a4f4b4da07808d1eb466397574c2210b7ab4bdd
vitu <vitorcsharp@hotmail.com>
2023-06-25 16:16:57 -0300

    create controller signUp page

---
+++ lib/view/sign_up/sign_up_controller.dart
@@ -0,0 +1,31 @@
+
+import 'package:finance/view/sign_up/sign_up_state.dart';
+import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+
+class SignUpController extends ChangeNotifier{
+   var logger = Logger();
+
+  SignUpState _state = SignUpInitialState();
+  SignUpState get state => _state;
+  void _changeState(SignUpState newState){
+    _state = newState;
+    notifyListeners();
+  }
+
+  Future<bool> doSignUp() async {
+    _changeState(SignUpLoadingState());
+   try {
+      await Future.delayed(Duration(seconds: 2));
+      throw Exception("Erro ao logar");
+    logger.i("usuario logado");
+
+    _changeState(SignUpSucessState());
+
+    return true;
+   } catch (e) {
+    _changeState(SignUpErrorState());
+    return false;
+   }
+  }
+}
