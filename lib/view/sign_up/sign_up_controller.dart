65f44391fb5f7069e889f84eeef39c51546303d8
vitu <vitorcsharp@hotmail.com>
2023-06-26 21:42:55 -0300

    add service on my controller

--- lib/view/sign_up/sign_up_controller.dart
+++ lib/view/sign_up/sign_up_controller.dart
@@ -1,31 +1,35 @@
-
+import 'package:finance/services/auth_service.dart';
 import 'package:finance/view/sign_up/sign_up_state.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 
-class SignUpController extends ChangeNotifier{
-   var logger = Logger();
+class SignUpController extends ChangeNotifier {
+  final AuthService _authService; //Repare que eu injeto a interface
+  SignUpController(this._authService);
+  var logger = Logger();
 
   SignUpState _state = SignUpInitialState();
   SignUpState get state => _state;
-  void _changeState(SignUpState newState){
+  void _changeState(SignUpState newState) {
     _state = newState;
     notifyListeners();
   }
 
-  Future<bool> doSignUp() async {
+  Future<void> signUp({required String name, required String email, required String password}) async {
     _changeState(SignUpLoadingState());
-   try {
-      await Future.delayed(Duration(seconds: 2));
-      // throw Exception("Erro ao logar"); //-> para simular um erro
-    logger.i("usuario logado");
+    try {
+      await _authService.signUp(
+        name: name,
+        email: email,
+        password: password,
+      );
 
-    _changeState(SignUpSucessState());
+      _changeState(SignUpSucessState());
 
-    return true;
-   } catch (e) {
-    _changeState(SignUpErrorState());
-    return false;
-   }
+    
+    } catch (e) {
+      _changeState(SignUpErrorState(e.toString()));
+      
+    }
   }
 }
