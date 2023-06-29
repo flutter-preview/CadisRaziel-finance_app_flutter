3c4e3359c509f8ec1300f36690ffc98958ba406c
vitu <vitorcsharp@hotmail.com>
2023-06-28 21:45:19 -0300

    add timer to navigation

--- lib/view/splash/splash_page.dart
+++ lib/view/splash/splash_page.dart
@@ -1,9 +1,33 @@
+import 'dart:async';
+
 import 'package:finance/core/shared/constants/app_colors.dart';
 import 'package:finance/core/shared/constants/app_text_style.dart';
+import 'package:finance/core/shared/routes/named_routes.dart';
+import 'package:finance/view/onboarding/onboarding_page.dart';
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+
+  Timer init(){
+    return Timer(const Duration(seconds: 2), navigateToOnboarding);
+  }
+
+  void navigateToOnboarding(){
+     Navigator.of(context).pushReplacementNamed(NamedRoute.initial);
+  }
+
+  @override
+  void initState() {
+    init();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
