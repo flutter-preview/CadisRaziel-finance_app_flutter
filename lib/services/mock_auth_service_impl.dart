9f228fa3eb9551c6d59f318b7dbefbaa378a9fd2
vitu <vitorcsharp@hotmail.com>
2023-06-28 21:42:34 -0300

    add one more method on my service implementation

--- lib/services/mock_auth_service_impl.dart
+++ lib/services/mock_auth_service_impl.dart
@@ -2,11 +2,7 @@ import 'package:finance/models/user_model.dart';
 import 'package:finance/services/auth_service.dart';
 
 class MockAuthServiceImpl implements AuthService {
-  @override
-  Future signIn() {
-    // TODO: implement signIn
-    throw UnimplementedError();
-  }
+
 
   @override
   Future<UserModel> signUp(
@@ -22,6 +18,22 @@ class MockAuthServiceImpl implements AuthService {
         throw "Senha fraca, digite uma senha mais forte";
       }
       throw "Não foi possível criar o usuário";
+    }
+  }
+
+  @override
+  Future<UserModel> signIn({required String email, required String password}) async {
+      await Future.delayed(Duration(seconds: 2));
+    try {
+      if (password.startsWith('123')) {
+        throw Exception();
+      }
+      return UserModel(id: email.hashCode, email: email);
+    } catch (e) {
+      if (password.startsWith('123')) {
+        throw "Erro ao logar";
+      }
+      throw "Não foi possível fazer o login";
     }
   }
 }
