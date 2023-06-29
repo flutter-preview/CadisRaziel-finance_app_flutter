d8dacb4b8dc0c21273851c9fe8c7cc12f208fb58
vitu <vitorcsharp@hotmail.com>
2023-06-28 21:41:34 -0300

    add configuration injection in class main

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,4 +1,6 @@
+import 'package:finance/core/shared/routes/named_routes.dart';
 import 'package:finance/view/onboarding/onboarding_page.dart';
+import 'package:finance/view/sign_in/sign_in_page.dart';
 import 'package:finance/view/sign_up/sign_up_page.dart';
 import 'package:finance/view/splash/splash_page.dart';
 import 'package:flutter/material.dart';
@@ -8,11 +10,14 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // theme: defaultTheme,
-      // home: const SplashPage(),
-      // home: const OnboardingPage(),
-      home: const SignUpPage(),
+    return MaterialApp(    
+      initialRoute:  NamedRoute.splash,
+      routes: {
+        NamedRoute.initial: (context) => const OnboardingPage(),
+        NamedRoute.splash: (context) => const SplashPage(),
+        NamedRoute.signUp: (context) => const SignUpPage(),
+        NamedRoute.signIn: (context) => const SignInPage(),
+      },
     );
   }
 }
