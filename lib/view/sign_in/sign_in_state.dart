43d0fcec3a9e7ccb6dd28a223e229211729ae084
vitu <vitorcsharp@hotmail.com>
2023-06-28 21:44:54 -0300

    create sign in state

---
+++ lib/view/sign_in/sign_in_state.dart
@@ -0,0 +1,12 @@
+abstract class SignInState {}
+
+class SignInInitialState extends SignInState {}
+
+class SignInLoadingState extends SignInState {}
+
+class SignInSucessState extends SignInState {}
+
+class SignInErrorState extends SignInState {
+  final String message;
+  SignInErrorState(this.message);
+}
