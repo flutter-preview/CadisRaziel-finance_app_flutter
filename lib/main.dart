8ac70477be972fb9cbeaf551736bfcabaaba169f
vitu <vitorcsharp@hotmail.com>
2023-06-28 21:40:53 -0300

    add configuration service injector on main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:finance/core/app.dart';
+import 'package:finance/locator.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  setUpDependencies();
   runApp(const App());
 }
