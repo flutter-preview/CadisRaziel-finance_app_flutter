e8e1b6b42b6d86bc5f7c0b9629b233509949072c
vitu <vitorcsharp@hotmail.com>
2023-06-28 21:42:14 -0300

    add one more method on my interface authservice

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -2,5 +2,5 @@ import 'package:finance/models/user_model.dart';
 
 abstract class AuthService {
   Future<UserModel> signUp({String? name, required String email, required String password});
-  Future signIn();
+  Future<UserModel> signIn({required String email, required String password});
 } 
