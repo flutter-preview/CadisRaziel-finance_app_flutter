8a912f705084fc218c00fc353e72cab67b00abab
vitu <vitorcsharp@hotmail.com>
2023-07-01 23:02:20 -0300

    add new route HomePage

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,4 +1,5 @@
 import 'package:finance/core/shared/routes/named_routes.dart';
+import 'package:finance/view/home/home_page.dart';
 import 'package:finance/view/onboarding/onboarding_page.dart';
 import 'package:finance/view/sign_in/sign_in_page.dart';
 import 'package:finance/view/sign_up/sign_up_page.dart';
@@ -17,6 +18,7 @@ class App extends StatelessWidget {
         NamedRoute.splash: (context) => const SplashPage(),
         NamedRoute.signUp: (context) => const SignUpPage(),
         NamedRoute.signIn: (context) => const SignInPage(),
+        NamedRoute.homePage: (context) => const HomePage(),
       },
     );
   }
