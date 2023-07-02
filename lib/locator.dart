f999e39db3a41fd6d0111d94272ea2f73e4cb151
vitu <vitorcsharp@hotmail.com>
2023-07-01 23:02:11 -0300

    registering the splashpage dependency factory with secureStorage in the locator

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,8 +1,10 @@
 import 'package:finance/services/auth_service.dart';
 import 'package:finance/services/firebase_auth_service.dart';
 import 'package:finance/services/mock_auth_service_impl.dart';
+import 'package:finance/services/secure_storage.dart';
 import 'package:finance/view/sign_in/sign_in_controller.dart';
 import 'package:finance/view/sign_up/sign_up_controller.dart';
+import 'package:finance/view/splash/splash_controller.dart';
 import 'package:get_it/get_it.dart';
 
 //aqui tera os seguintes objetos -> Services / Controllers
@@ -13,6 +15,10 @@ void setUpDependencies() {
   //passo 1º a classe abstrata e depois a classe de implementação da abstrata
   //registerLazySingleton -> criar o objeto apenas quando for necessario e não desde o inicio da aplicação
   locator.registerLazySingleton<AuthService>(() => (FirebaseAuthService()));
+
+//como eu deixei o construtor do SecureStorage const é só eu criar um registerFactory
+  locator.registerFactory<SplashController>(
+      () => SplashController(const SecureStorage()));
 
   //registerFactory -> cria um objeto novo toda vez que chamar o 'locator.get()'
   //sempre que precisarmos de um novo objeto do tipo SignInController nos chamamos ele mesmo
