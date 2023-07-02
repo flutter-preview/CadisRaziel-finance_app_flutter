cfaca91c26563dcc5d360e8f30a07927b3b55420
vitu <vitorcsharp@hotmail.com>
2023-07-01 23:04:28 -0300

    comment mockservice

--- lib/services/mock_auth_service_impl.dart
+++ lib/services/mock_auth_service_impl.dart
@@ -1,39 +1,39 @@
-import 'package:finance/models/user_model.dart';
-import 'package:finance/services/auth_service.dart';
+// import 'package:finance/models/user_model.dart';
+// import 'package:finance/services/auth_service.dart';
 
-class MockAuthServiceImpl implements AuthService {
+// class MockAuthServiceImpl implements AuthService {
 
 
-  @override
-  Future<UserModel> signUp(
-      {String? name, required String email, required String password}) async {
-      await Future.delayed(Duration(seconds: 2));
-    try {
-      if (password.startsWith('123')) {
-        throw Exception();
-      }
-      return UserModel(id: email.hashCode.toString(), name: name, email: email);
-    } catch (e) {
-      if (password.startsWith('123')) {
-        throw "Senha fraca, digite uma senha mais forte";
-      }
-      throw "Não foi possível criar o usuário";
-    }
-  }
+//   @override
+//   Future<UserModel> signUp(
+//       {String? name, required String email, required String password}) async {
+//       await Future.delayed(Duration(seconds: 2));
+//     try {
+//       if (password.startsWith('123')) {
+//         throw Exception();
+//       }
+//       return UserModel(id: email.hashCode.toString(), name: name, email: email);
+//     } catch (e) {
+//       if (password.startsWith('123')) {
+//         throw "Senha fraca, digite uma senha mais forte";
+//       }
+//       throw "Não foi possível criar o usuário";
+//     }
+//   }
 
-  @override
-  Future<UserModel> signIn({required String email, required String password}) async {
-      await Future.delayed(Duration(seconds: 2));
-    try {
-      if (password.startsWith('123')) {
-        throw Exception();
-      }
-      return UserModel(id: email.toString(), email: email);
-    } catch (e) {
-      if (password.startsWith('123')) {
-        throw "Erro ao logar";
-      }
-      throw "Não foi possível fazer o login";
-    }
-  }
-}
+//   @override
+//   Future<UserModel> signIn({required String email, required String password}) async {
+//       await Future.delayed(Duration(seconds: 2));
+//     try {
+//       if (password.startsWith('123')) {
+//         throw Exception();
+//       }
+//       return UserModel(id: email.toString(), email: email);
+//     } catch (e) {
+//       if (password.startsWith('123')) {
+//         throw "Erro ao logar";
+//       }
+//       throw "Não foi possível fazer o login";
+//     }
+//   }
+// }
